1f99df56850e6fd49f66691c69ec8523ab6b7e1c
Nihat-24 <nihatagayew@gmail.com>
2025-11-17 13:43:04 +0300

    deis

---
+++ app.js
@@ -0,0 +1,10 @@
+// document.write("nihat agayev <br>")
+// document.write("nihat agayev")
+
+console.log("Hello world")
+console.clear
+console.log(window)
+// alert("Salam")
+
+var aga = 10;
+console.log(aga)
